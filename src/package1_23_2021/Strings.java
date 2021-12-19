18fc5f84712a326853124c0ca9da63fb1e961b60
= <=>
2021-12-19 00:30:29 -0500

    Strings

---
+++ src/package1_23_2021/Strings.java
@@ -0,0 +1,26 @@
+package package1_23_2021;
+
+public class Strings {
+    public static void main(String[] args) {
+        
+        int myInt = 7;
+        
+        String text = "Hello";
+        
+        String blank = " ";
+        
+        String name = "Bob";
+        
+        String greeting = text + blank + name;
+        
+        System.out.println(greeting);
+        
+        System.out.println("Hello" + " " + "Bob");
+        
+        System.out.println("My integer is: " + myInt);
+        
+        double myDouble = 7.8;
+        
+        System.out.println("My number is: " + myDouble + ".");
+    }
+}
