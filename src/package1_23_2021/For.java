93dd7f3809a0d8993ecae6ad2dc177250a35529a
= <=>
2021-12-19 00:34:30 -0500

    For Loop

---
+++ src/package1_23_2021/For.java
@@ -0,0 +1,10 @@
+package package1_23_2021;
+
+public class For {
+    public static void main(String[] args) {
+        
+        for(int i=0; i < 5; i++) {
+            System.out.printf("The value of i is: %dn", i);
+        }
+    }
+}
