ea11eed99d22d1271c5455339913ecc2909d5139
= <=>
2021-12-19 00:33:40 -0500

    While

---
+++ src/package1_23_2021/While.java
@@ -0,0 +1,15 @@
+package package1_23_2021;
+
+public class While {
+    public static void main(String[] args) {
+        
+        int value = 0;
+    
+        while(value < 10)
+        {
+            System.out.println("Hello " + value);
+            
+            value = value + 1;
+        }
+    }
+}
