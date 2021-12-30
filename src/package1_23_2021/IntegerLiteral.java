959eb0c7c6c3f2ab25d16864ab5083df13132a7c
= <=>
2021-12-30 01:48:21 -0500

    Integer Literals

---
+++ src/package1_23_2021/IntegerLiteral.java
@@ -0,0 +1,15 @@
+package package1_23_2021;
+
+public class IntegerLiteral {
+
+	public static void main(String[] args) {
+//		By default, an integer literal is a decimal integer number. To denote a binary integer 
+//		literal, use a leading 0b or 0B (zero B), to denote an octal integer literal, use a leading 
+//		0 (zero), and to denote a hexadecimal integer literal, use a leading 0x or 0X (zero X). 
+//		For example, 
+		System.out.println(0B1111); // Displays 15
+		System.out.println(07777); // Displays 4095
+		System.out.println(0XFFFF); // Displays 65535
+	}
+
+}
